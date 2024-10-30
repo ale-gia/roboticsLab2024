ee5dc97acfb29c3739b7d30cfe2c62e5639ed1be
daniele508 <danielefranco00@icloud.com>
2024-10-30 10:42:27 +0100

    movement completo

--- qi_unipa/qi_unipa/qi_unipa_movement.py
+++ qi_unipa/qi_unipa/qi_unipa_movement.py
@@ -3,16 +3,19 @@ import rclpy
 import argparse
 from rclpy.node import Node
 import sys
-from std_msgs.msg import Int32, Float32MultiArray,String
-import time 
+from std_msgs.msg import Int32
+from geometry_msgs.msg import Pose2D
+from qi_unipa_msgs.msg import PostureWithSpeed
 
-class QiUnipa_Movement(Node):
+class QiUnipa(Node):
     def __init__(self, args):
-        super().__init__('qi_unipa_movement')
+        super().__init__('qi_unipa')
         self.session = self.set_connection(args)
-        self.state_sub = self.create_subscription(Int32, "/state", self.set_state, 10)
-        self.walkTo_sub = self.create_subscription(Float32MultiArray, "/walkTo", self.walkTo, 10)
-        #self.walkTo_sub = self.create_subscription(String, "/listen", self.listen, 10)
+        self.subscription = self.create_subscription(Int32, "/state", self.set_state, 10)
+        self.subscription = self.create_subscription(Int32, "/joint_angles_with_speed", self.set_joint_angles_with_speed, 10)
+        self.subscription = self.create_subscription(Pose2D, "/walk", self.set_walking, 10)
+        self.subscription = self.create_subscription(PostureWithSpeed, "/posture", self.set_posture, 10)
+
 
     def set_connection(self, args):
         session = qi.Session()
@@ -31,54 +34,45 @@ class QiUnipa_Movement(Node):
             state_service.wakeUp()
         else:
             state_service.rest()
-    
 
-    def walkTo(self,msg):
-        x,y,theta=msg.data
+    def set_joint_angles_with_speed(self, msg):
+        names = msg.names
+        angles = msg.angles
+        speed = msg.speed
+        joints_service = self.session.service("ALMotion")
+        joints_service.angleInterpolationWithSpeed(names, angles, speed)
+
+    def set_walking(self, msg):
+        x = msg.x
+        y = msg.y
+        theta = msg.theta
         walk_service = self.session.service("ALMotion")
         walk_service.moveTo(x,y,theta)
 
-    def speech_rec(self):
-        speech_service = self.session.service("ALSpeechRecognition")
-        memory_service = self.session.service("ALMemory")
-        speech_service.setLanguage("Italian")
-        keywords = ["ciao", "Pepper", "come stai", "aiuto"]
-        speech_service.setVocabulary(keywords, False)
-        memory_service.subscribeToEvent("WordRecognized", "Test_ASR", "self.onWordRecognized")
-        speech_service.subscribe("Test_ASR")
-                # Mantieni attivo il riconoscimento per un certo periodo di tempo
-        try:
-            time.sleep(20)  # Riconosce per 20 secondi, modifica se necessario
-        finally:
-            # Interrompi il riconoscimento
-            speech_service.unsubscribe("Test_ASR")
-            memory_service.unsubscribeToEvent("WordRecognized","Test_ASR", "self.onWordRecognized")
+    def set_posture(self, msg):
+        pose_name = msg.postureName
+        speed = msg.speed
+        posture_service = self.session.service("AlRobotPosture")
+        posture_service.goToPosture(pose_name,speed)
+    
 
-
-    # Funzione per gestire l'output del riconoscimento vocale
-    def onWordRecognized(self,value):
-        if isinstance(value, list) and len(value) > 1:
-            word = value[0]
-            confidence = value[1]
-            print(f"Parola riconosciuta: {word} con confidenza: {confidence}")
 
 def main(args=None):
-    rclpy.init(args=args)
+    
     parser = argparse.ArgumentParser()
-    parser.add_argument("--ip", type=str, default="192.168.0.161",
+    parser.add_argument("--ip", type=str, default="127.0.0.1",
                         help="Robot IP address. On robot or Local Naoqi: use '127.0.0.1'.")
     parser.add_argument("--port", type=int, default=9559,
                         help="Naoqi port number")
     
     args = parser.parse_args()
 
+    rclpy.init(args=args)
+    node = QiUnipa(args)
     
-    node = Qi_Unipa_controller(args)
-    node.speech_rec()
     rclpy.spin(node)
     rclpy.shutdown()
     
-       
 
 if __name__ == '__main__':
     main()
