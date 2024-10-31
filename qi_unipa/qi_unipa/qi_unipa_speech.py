b494601585ca2efdb0fd3c39d85da744c5310c16
ale-gia <nola@live.it>
2024-10-31 13:00:38 +0100

    speech_rec v0.2

--- qi_unipa/qi_unipa/qi_unipa_speech.py
+++ qi_unipa/qi_unipa/qi_unipa_speech.py
@@ -3,15 +3,16 @@ import time
 import sys
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Bool
+from std_msgs.msg import Bool,String
 
 class QiUnipaSpeech(Node):  
     def __init__(self, robot_ip, robot_port=9559):
         super().__init__('qi_unipa_speech')
         
         self.is_recognizing = False
+        self.last_word=""
         self.vocabulary = ["ciao", "come stai", "arrivederci"]
-        
+        self.reply={"ciao":"Ciao a te","come stai": " Io sto bene e tu come ti senti?"}
         self.session = qi.Session()
         try:
             self.session.connect(f"tcp://{robot_ip}:{robot_port}")
@@ -23,12 +24,15 @@ class QiUnipaSpeech(Node):
       
         self.asr_service = self.session.service("ALSpeechRecognition")
         self.memory = self.session.service("ALMemory")
+       
+        self.tts_service= self.session.service("ALTextToSpeech")
         self.speech_sub = self.create_subscription(Bool, "/speech_start", self.speech_callback, 10)
+        self.tts_sub = self.create_subscription(String, "/tts", self.tts, 10)
         # Setup iniziale del riconoscimento vocale
         self.setup_recognition()
         
         # Timer per il controllo periodico del riconoscimento vocale
-        self.create_timer(0.1, self.check_recognition)
+        self.create_timer(1, self.check_recognition)
 
     def speech_callback(self, msg):
         """Callback per il topic /speech"""
@@ -44,6 +48,7 @@ class QiUnipaSpeech(Node):
         try:
             # Imposta la lingua in italiano
             self.asr_service.setLanguage("Italian")
+            self.tts_service.setLanguage("Italian")
             print("Lingua impostata a Italiano")
             
             # Imposta il vocabolario
@@ -86,13 +91,30 @@ class QiUnipaSpeech(Node):
             
         try:
             word_data = self.memory.getData("WordRecognized")
+      
             if word_data and len(word_data) >= 2:
                 word = word_data[0]
                 confidence = word_data[1]
-                if confidence > 0.5:
+                if confidence > 0.5 and word!=self.last_word:
                     print(f"Parola riconosciuta: {word} (confidenza: {confidence})")
+                    self.answers(word)
+                    self.last_word=word
         except Exception as e:
             print(f"Errore durante il controllo del riconoscimento: {e}")
+
+
+        
+    def tts(self, msg):
+        
+        self.tts_service.say(msg.data)
+
+    def answers(self,word):
+        
+        if word  in self.reply:
+            print(f'risposta :{self.reply[word]}')
+            ans=self.reply[word]
+            self.tts_service.say(ans)
+
 
 def main(args=None):
     rclpy.init(args=args)
