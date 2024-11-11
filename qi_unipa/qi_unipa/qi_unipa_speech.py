9a0903d4fb06ad801f788d5a0af80e81bfda2f6f
ale-gia <nola@live.it>
2024-11-11 12:21:40 +0100

    update speech and tracking

--- qi_unipa/qi_unipa/qi_unipa_speech.py
+++ qi_unipa/qi_unipa/qi_unipa_speech.py
@@ -1,6 +1,7 @@
 import qi
 import sys
 import rclpy
+import re
 from rclpy.node import Node
 from std_msgs.msg import Bool,String
 from qi_unipa_msgs.msg import PostureWithSpeed,Track
@@ -20,19 +21,19 @@ class QiUnipaSpeech(Node):
 
         self.is_recognizing = False
         self.last_word=""
-        self.vocabulary = ["ciao", "come stai", "stop", 
+        self.vocabulary = ["ciao", "come stai", "stop parlato", 
                            "gruppo di formiche","troppo rumoroso","nuova scoperta",
-                           "potremmo differenziare","funzionano le cose","di ragazze",
+                           "potremmo differenziare","le cose","di ragazze",
                            "colorato assegnato"]
         
-        self.reply={"ciao":"Ciao ^start(animations/Stand/BodyTalk/BodyTalk_1) a te ^wait(animations/Stand/BodyTalk/BodyTalk_1) ",
+        self.reply={"ciao":"Ciao ^start(animations/Stand/BodyTalk/BodyTalk_1) io sono Pepper, piacere di conoscerti ^wait(animations/Stand/BodyTalk/BodyTalk_1) ",
                     "come stai": " Io sto bene e tu come ti senti?",
-                    "stop":"Ciao Ciao",
+                    "stop parlato":"Ciao Ciao",
                     "gruppo di formiche": "Anche ^start(animations/Stand/Emotions/Positive/Peaceful_1) io, Simone. Le formiche sono affascinanti! Hanno una società così complessa...^wait(animations/Stand/Emotions/Positive/Peaceful_1)",
                     "troppo rumoroso":"Forse ^start(animations/Stand/BodyTalk/BodyTalk_8) potremmo aiutarla a trovare un po' di ordine, no, Simone?^wait(animations/Stand/BodyTalk/BodyTalk_8)",
                     "nuova scoperta":"Esatto ^start(animations/Stand/Emotions/Positive/Hysterical_1)! è come un'avventura. Si esplorano nuovi mondi, si fanno nuove scoperte, si imparano nuove cose...^wait(animations/Stand/Emotions/Positive/Hysterical_1)",
                     "potremmo differenziare":"Ottima ^start(animations/Stand/Gestures/Enthusiastic_4) idea! Potrei analizzare i codici a barre delle confezioni e suggerire il contenitore più adatto.^wait(animations/Stand/Gestures/Enthusiastic_4)",
-                    "funzionano le cose":"Potrei ^start(animations/Stand/Gestures/Thinking_1) mostrarti le immagini di un cervello che prova il piacere della scoperta, ma temo che nessuno qui reggerebbe la tua espressione di perplessità. ^wait(animations/Stand/Gestures/Thinking_1)",
+                    "le cose":"Potrei ^start(animations/Stand/Gestures/Thinking_1) mostrarti le immagini di un cervello che prova il piacere della scoperta, ma temo che nessuno qui reggerebbe la tua espressione di perplessità. ^wait(animations/Stand/Gestures/Thinking_1)",
                     "di ragazze":"In ^start(animations/Stand/Gestures/Desperate_1)effetti, non posso darti torto. Potresti darmi una mano tu! ^wait(animations/Stand/Gestures/Desperate_1)",
                     "colorato assegnato":"Pensate ^start(animations/Stand/Emotions/Positive/Happy_4)che io sono stato programmato allo stesso modo, ma il mio algoritmo è molto più complesso!^wait(animations/Stand/Emotions/Positive/Happy_4)"}
         
@@ -41,10 +42,10 @@ class QiUnipaSpeech(Node):
         self.memory = self.session.service("ALMemory")
         self.animated_service = self.session.service("ALAnimatedSpeech")
         self.tts_service= self.session.service("ALTextToSpeech")
-        #self.configuration = {"bodyLanguageMode":"contextual"}
+        self.configuration = {"bodyLanguageMode":"contextual"}
         
-        self.speech_sub = self.create_subscription(Bool, "/listen", self.speech_callback, 10)
-        self.tts_sub = self.create_subscription(String, "/speak", self.tts, 10)
+        self.speech_sub = self.create_subscription(Bool, "/listen", self.set_speech, 10)
+        self.tts_sub = self.create_subscription(String, "/speak", self.set_tts, 10)
         self.tracking_pub = self.create_publisher(Track, "/track", 10)
         self.posture_pub = self.create_publisher(PostureWithSpeed, "/posture", 10)
 
@@ -52,7 +53,7 @@ class QiUnipaSpeech(Node):
         self.setup_recognition()
         
         # Timer per il controllo periodico del riconoscimento vocale
-        self.create_timer(1.5, self.check_recognition)
+        self.create_timer(1.0, self.check_recognition)
 
     def set_connection(self, ip, port):
         session = qi.Session()
@@ -64,7 +65,7 @@ class QiUnipaSpeech(Node):
             sys.exit(1)
         return session
     
-    def speech_callback(self, msg):
+    def set_speech(self, msg):
         """Callback per il topic /speech"""
         if msg.data:
             if not self.is_recognizing:
@@ -83,7 +84,7 @@ class QiUnipaSpeech(Node):
             self.get_logger().info("Lingua impostata a Italiano")
             
             # Imposta il vocabolario
-            self.asr_service.setVocabulary(self.vocabulary, False)
+            self.asr_service.setVocabulary(self.vocabulary, True)
             self.get_logger().info(f"Vocabolario impostato: {self.vocabulary}")
             
             # Creazione del proxy per l'evento
@@ -139,27 +140,32 @@ class QiUnipaSpeech(Node):
             
         try:
             word_data = self.memory.getData("WordRecognized")
-            self.memory.insertData("WordRecognized","")
-            if word_data and len(word_data) >= 2:
-                word = word_data[0]
+            self.get_logger().info(f"Sentence: {word_data}")
+            self.memory.insertData("WordRecognized","") #evita di leggere ripetutamente la stessa parola
+            if len(word_data[0])>0:
+                word =re.sub(r'\W+', ' ', word_data[0]).strip() # Regex per eliminare tutto ciò che non è una parola
+
+            if word_data and word_data!=self.last_word:
+                
                 confidence = word_data[1]
-              
-                if confidence > 0.45 :
-                    self.get_logger().info(f"Parola riconosciuta: {word} (confidenza: {confidence})")
+
+                self.get_logger().info(f"Parola riconosciuta: {word} (confidenza: {confidence})")
+                self.last_word=word
+                if confidence > 0.455 :
+                    #self.get_logger().info(f"Parola riconosciuta: {word} (confidenza: {confidence})")
                     self.answers(word)
-                 
+                    
         except Exception as e:
              self.get_logger().info(f"Errore durante il controllo del riconoscimento: {e}")
 
 
         
-    def tts(self, msg):
+    def set_tts(self, msg):
         
-        self.tts_service.say(msg.data)
+        self.animated_service.say(msg.data)
 
     def answers(self,word):
        
-
         if word  in self.reply:
             self.get_logger().info(f'risposta :{self.reply[word]}')
             ans=self.reply[word]
@@ -168,7 +174,7 @@ class QiUnipaSpeech(Node):
             msg.posture_name="StandInit"
             msg.speed=0.5
             self.posture_pub.publish(msg)
-            if word =="stop" :
+            if word =="stop parlato" :
                 self.stop_recognition()
                 
 
