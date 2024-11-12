21edd26f1c0d7782914a5d0725caa3952a28479e
Antonios99 <sciacchitanoantonio90@gmail.com>
2024-11-12 17:44:38 +0100

    aa

--- qi_unipa/qi_unipa/qi_unipa_vision.py
+++ qi_unipa/qi_unipa/qi_unipa_vision.py
@@ -41,7 +41,7 @@ class QiUnipa_vision(Node):
         self.face_service = self.session.service("ALFaceDetection")
         self.memory_service = self.session.service("ALMemory")
         
-        self.camera_timer = self.create_timer(1, self.get_camera)
+        self.camera_timer = self.create_timer(0.1, self.get_camera)
         
     def set_connection(self, ip, port):
         session = qi.Session()
